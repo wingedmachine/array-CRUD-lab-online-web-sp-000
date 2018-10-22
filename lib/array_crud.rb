1a8688a021e0f16131b8e200d57f8266255d2c52
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 20:58:50 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
   array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
+def retrieve_element_from_index(array, index)
   array[index]
 end
 
